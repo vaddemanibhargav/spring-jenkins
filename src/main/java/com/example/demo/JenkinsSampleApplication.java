e8a460c59441690748fe91ee34373ddd1e846922
vaddemaniBhargav <vaddemanibhargavreddy143@gmail.com>
2024-10-21 10:34:27 +0530

    SChanged

--- src/main/java/com/example/demo/JenkinsSampleApplication.java
+++ src/main/java/com/example/demo/JenkinsSampleApplication.java
@@ -1,21 +1,16 @@
 package com.example.demo;
 
-import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import ch.qos.logback.classic.Logger;
-import jakarta.annotation.PostConstruct;
+
 
 @SpringBootApplication
 public class JenkinsSampleApplication {
-	public static org.slf4j.Logger logger=LoggerFactory.getLogger(JenkinsSampleApplication.class);
-	@PostConstruct
-	public void init() {
-		logger.info("aplication started...");
-	}
+	
 	public static void main(String[] args) {
-		logger.info("Application Executed...");
+		System.out.println("Hello Things");
 		SpringApplication.run(JenkinsSampleApplication.class, args);
 	}
 
